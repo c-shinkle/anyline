d4479e6edfb5d82bafddd6f9f071018932c26562
Christian Shinkle <cshinkle2018@gmail.com>
2025-06-30 06:58:01 -0500

    First prototype is working! implemented left arrow

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,20 @@
 pub fn main() !void {
     const stdout_writer = std.io.getStdOut().writer();
-    var stdin_reader = std.io.getStdIn().reader();
+    var stdin_file = std.io.getStdIn();
+    const stdin_reader = stdin_file.reader();
+
+    var old: termios.termios = undefined;
+    _ = termios.tcgetattr(stdin_file.handle, &old);
+    defer _ = termios.tcsetattr(stdin_file.handle, termios.TCSANOW, &old);
+
+    var new = old;
+    const ICANON: c_uint = @as(c_uint, termios.ICANON);
+    const ECHO: c_uint = @as(c_uint, termios.ECHO);
+    new.c_lflag &= ~(ICANON | ECHO);
+    _ = termios.tcsetattr(stdin_file.handle, termios.TCSANOW, &new);
+
     // var row: usize = 0;
-    var col: usize = 0;
+    var col_offset: usize = 0;
     const prompt = ">> ";
     var gpa = std.heap.DebugAllocator(.{}){};
     const allocator = gpa.allocator();
@@ -19,9 +31,8 @@ pub fn main() !void {
     while (true) {
         // Start new line
         try stdout_writer.writeAll(prompt);
-
         var line_buffer: std.ArrayListUnmanaged(u8) = .empty;
-        byte_loop: while (true) {
+        while (true) {
             const byte = try stdin_reader.readByte();
             if (byte == std.ascii.control_code.esc) {
                 const second_byte = try stdin_reader.readByte();
@@ -29,9 +40,9 @@ pub fn main() !void {
                 const third_byte = try stdin_reader.readByte();
                 switch (third_byte) {
                     'D' => {
-                        col -|= 1;
-                        try cursor.setCursorColumn(stdout_writer, col);
-                        break :byte_loop;
+                        col_offset -|= 1;
+                        try cursor.setCursorColumn(stdout_writer, prompt.len + col_offset);
+                        continue;
                     },
                     // 'C' => try stdout_writer.print("Right\n", .{}),
                     // 'A' => try stdout_writer.print("Up\n", .{}),
@@ -39,15 +50,25 @@ pub fn main() !void {
                     else => @panic("unhandled control byte"),
                 }
             } else if (byte == '\n') {
-                break :byte_loop;
+                try stdout_writer.writeByte('\n');
+                try history_buffer.append(arena, try line_buffer.toOwnedSlice(arena));
+                col_offset = 0;
+                break;
             } else {
-                // try stdout_writer.writeAll("append\n");
-                try line_buffer.append(arena, byte);
-                col += 1;
+                try cursor.setCursorColumn(stdout_writer, prompt.len);
+                try ansi_term.clear.clearFromCursorToLineEnd(stdout_writer);
+
+                if (col_offset < line_buffer.items.len) {
+                    line_buffer.items[col_offset] = byte;
+                } else {
+                    try line_buffer.append(arena, byte);
+                }
+                col_offset += 1;
+                try stdout_writer.writeAll(line_buffer.items);
+                try cursor.setCursorColumn(stdout_writer, prompt.len + col_offset);
             }
         }
         // Finish line
-        try history_buffer.append(arena, try line_buffer.toOwnedSlice(arena));
         // try stdout_writer.writeByte('\n');
         // row += 1;
     }
@@ -56,3 +77,4 @@ pub fn main() !void {
 const std = @import("std");
 const ansi_term = @import("ansi_term");
 const cursor = ansi_term.cursor;
+const termios = @cImport(@cInclude("termios.h"));
