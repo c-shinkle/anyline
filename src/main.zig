c5b0101675294d08b6b3bfa34049a5a79a923b03
Christian Shinkle <cshinkle2018@gmail.com>
2025-06-30 21:59:06 -0500

    Added backspace

--- src/main.zig
+++ src/main.zig
@@ -1,17 +1,17 @@
 pub fn main() !void {
     const stdout_writer = std.io.getStdOut().writer();
-    var stdin_file = std.io.getStdIn();
-    const stdin_reader = stdin_file.reader();
+    const stdin_reader = std.io.getStdIn().reader();
+    const stdin_handle = std.io.getStdIn().handle;
 
     var old: termios.termios = undefined;
-    _ = termios.tcgetattr(stdin_file.handle, &old);
-    defer _ = termios.tcsetattr(stdin_file.handle, termios.TCSANOW, &old);
+    _ = termios.tcgetattr(stdin_handle, &old);
+    defer _ = termios.tcsetattr(stdin_handle, termios.TCSANOW, &old);
 
     var new = old;
     const ICANON: c_uint = @as(c_uint, termios.ICANON);
     const ECHO: c_uint = @as(c_uint, termios.ECHO);
     new.c_lflag &= ~(ICANON | ECHO);
-    _ = termios.tcsetattr(stdin_file.handle, termios.TCSANOW, &new);
+    _ = termios.tcsetattr(stdin_handle, termios.TCSANOW, &new);
 
     // var row: usize = 0;
     var col_offset: usize = 0;
@@ -28,32 +28,53 @@ pub fn main() !void {
     try cursor.setCursorRow(stdout_writer, 0);
     try cursor.setCursorColumn(stdout_writer, 0);
 
+    // line_loop:
     while (true) {
         // Start new line
         try stdout_writer.writeAll(prompt);
         var line_buffer: std.ArrayListUnmanaged(u8) = .empty;
-        while (true) {
+        byte_loop: while (true) {
             const byte = try stdin_reader.readByte();
             if (byte == std.ascii.control_code.esc) {
                 const second_byte = try stdin_reader.readByte();
                 std.debug.assert(second_byte == '[');
                 const third_byte = try stdin_reader.readByte();
                 switch (third_byte) {
-                    'D' => {
+                    'D' => { //Left
                         col_offset -|= 1;
                         try cursor.setCursorColumn(stdout_writer, prompt.len + col_offset);
-                        continue;
                     },
-                    // 'C' => try stdout_writer.print("Right\n", .{}),
+                    'C' => { //Right
+                        col_offset = @min(col_offset + 1, line_buffer.items.len);
+                        try cursor.setCursorColumn(stdout_writer, prompt.len + col_offset);
+                    },
                     // 'A' => try stdout_writer.print("Up\n", .{}),
                     // 'B' => try stdout_writer.print("Down\n", .{}),
+                    // TODO handle back space
                     else => @panic("unhandled control byte"),
                 }
             } else if (byte == '\n') {
                 try stdout_writer.writeByte('\n');
                 try history_buffer.append(arena, try line_buffer.toOwnedSlice(arena));
                 col_offset = 0;
-                break;
+                break :byte_loop;
+            } else if (byte == std.ascii.control_code.del) { //Backspace
+                if (col_offset == 0) {
+                    continue;
+                }
+
+                if (line_buffer.items.len == col_offset - 1) {
+                    line_buffer.items.len -= 1;
+                } else {
+                    line_buffer.replaceRangeAssumeCapacity(col_offset - 1, 1, &.{});
+                }
+                col_offset -|= 1;
+
+                try cursor.setCursorColumn(stdout_writer, prompt.len);
+                try ansi_term.clear.clearFromCursorToLineEnd(stdout_writer);
+
+                try stdout_writer.writeAll(line_buffer.items);
+                try cursor.setCursorColumn(stdout_writer, prompt.len + col_offset);
             } else {
                 try cursor.setCursorColumn(stdout_writer, prompt.len);
                 try ansi_term.clear.clearFromCursorToLineEnd(stdout_writer);
@@ -63,6 +84,7 @@ pub fn main() !void {
                 } else {
                     try line_buffer.append(arena, byte);
                 }
+
                 col_offset += 1;
                 try stdout_writer.writeAll(line_buffer.items);
                 try cursor.setCursorColumn(stdout_writer, prompt.len + col_offset);
