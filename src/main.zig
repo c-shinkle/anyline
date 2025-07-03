82c8f61d9f18666f05ff2cf6551c38ca12a7ef1c
Christian Shinkle <cshinkle2018@gmail.com>
2025-07-02 21:59:05 -0500

    added error handling for termios, reordered switch prongs

--- src/main.zig
+++ src/main.zig
@@ -1,21 +1,35 @@
 pub fn main() !void {
     const stdout_writer = std.io.getStdOut().writer();
+    const stderr_writer = std.io.getStdErr().writer();
     const stdin_reader = std.io.getStdIn().reader();
     const stdin_handle = std.io.getStdIn().handle;
 
     var old = old: {
         var temp: termios.termios = undefined;
-        _ = termios.tcgetattr(stdin_handle, &temp);
+        if (termios.tcgetattr(stdin_handle, &temp) < 0) {
+            const errno_val = std.c._errno().*;
+            const errno_string = string.strerror(errno_val);
+            try stderr_writer.print("{s}\n", .{errno_string});
+            return;
+        }
 
         var new = temp;
         const ICANON = @as(c_uint, termios.ICANON);
         const ECHO = @as(c_uint, termios.ECHO);
         new.c_lflag &= ~(ICANON | ECHO);
-        // TODO check c_int
-        _ = termios.tcsetattr(stdin_handle, termios.TCSANOW, &new);
+        if (termios.tcsetattr(stdin_handle, termios.TCSANOW, &new) < 0) {
+            const errno_val = std.c._errno().*;
+            const errno_string = string.strerror(errno_val);
+            try stderr_writer.print("{s}\n", .{errno_string});
+            return;
+        }
         break :old temp;
     };
-    defer _ = termios.tcsetattr(stdin_handle, termios.TCSANOW, &old);
+    defer if (termios.tcsetattr(stdin_handle, termios.TCSANOW, &old) < 0) {
+        const errno_val = std.c._errno().*;
+        const errno_string = string.strerror(errno_val);
+        stderr_writer.print("{s}\n", .{errno_string}) catch {};
+    };
 
     var gpa = std.heap.DebugAllocator(.{}){};
     const child_allocator = gpa.allocator();
@@ -30,45 +44,43 @@ pub fn main() !void {
     var history_buffer: std.ArrayListUnmanaged([]const u8) = .empty;
 
     try ansi_term.clear.clearScreen(stdout_writer);
-    try cursor.setCursorRow(stdout_writer, 0);
-    try cursor.setCursorColumn(stdout_writer, 0);
 
     line_loop: while (true) {
+        try cursor.setCursorRow(stdout_writer, row);
+        try cursor.setCursorColumn(stdout_writer, col_offset);
         try stdout_writer.writeAll(prompt);
+
         var line_buffer: std.ArrayListUnmanaged(u8) = .empty;
         try history_buffer.append(arena, line_buffer.items);
         byte_loop: while (true) {
-            const byte = try stdin_reader.readByte();
-            switch (byte) {
+            switch (try stdin_reader.readByte()) {
                 control_code.eot => break :line_loop,
+                control_code.lf => {
+                    const finished_line = try line_buffer.toOwnedSlice(arena);
+                    history_buffer.items[history_buffer.items.len - 1] = finished_line;
+                    row += 1;
+                    col_offset = 0;
+                    history_index = history_buffer.items.len;
+                    break :byte_loop;
+                },
                 control_code.esc => {
                     std.debug.assert('[' == try stdin_reader.readByte());
                     const third_byte = try stdin_reader.readByte();
                     switch (third_byte) {
-                        'D' => { //Left
-                            col_offset -|= 1;
-                            try cursor.setCursorColumn(stdout_writer, prompt.len + col_offset);
-                        },
-                        'C' => { //Right
-                            col_offset = @min(col_offset + 1, line_buffer.items.len);
-                            try cursor.setCursorColumn(stdout_writer, prompt.len + col_offset);
-                        },
                         'A' => { //Up
                             if (history_index == 0) {
                                 continue;
                             }
 
-                            std.debug.assert(history_buffer.items.len > 0);
-                            if (history_index == history_buffer.items.len - 1) {
+                            if (history_index + 1 == history_buffer.items.len) {
                                 history_buffer.items[history_index] = try line_buffer.toOwnedSlice(arena);
                             }
                             history_index -|= 1;
                             line_buffer.clearRetainingCapacity();
                             try line_buffer.appendSlice(arena, history_buffer.items[history_index]);
 
-                            try cursor.setCursor(stdout_writer, 0, row);
+                            try cursor.setCursor(stdout_writer, prompt.len, row);
                             try ansi_term.clear.clearFromCursorToLineEnd(stdout_writer);
-                            try stdout_writer.writeAll(prompt);
                             try stdout_writer.writeAll(line_buffer.items);
                             col_offset = line_buffer.items.len;
                         },
@@ -77,27 +89,25 @@ pub fn main() !void {
                                 continue;
                             }
 
-                            line_buffer.clearRetainingCapacity();
                             history_index += 1;
+                            line_buffer.clearRetainingCapacity();
                             try line_buffer.appendSlice(arena, history_buffer.items[history_index]);
 
-                            try cursor.setCursor(stdout_writer, 0, row);
+                            try cursor.setCursor(stdout_writer, prompt.len, row);
                             try ansi_term.clear.clearFromCursorToLineEnd(stdout_writer);
-                            try stdout_writer.writeAll(prompt);
                             try stdout_writer.writeAll(line_buffer.items);
                             col_offset = line_buffer.items.len;
                         },
+                        'C' => { //Right
+                            col_offset = @min(col_offset + 1, line_buffer.items.len);
+                            try cursor.setCursorColumn(stdout_writer, prompt.len + col_offset);
+                        },
+                        'D' => { //Left
+                            col_offset -|= 1;
+                            try cursor.setCursorColumn(stdout_writer, prompt.len + col_offset);
+                        },
                         else => @panic("unhandled control byte"),
                     }
-                },
-                control_code.lf => {
-                    try stdout_writer.writeByte('\n');
-                    std.debug.assert(history_buffer.items.len > 0);
-                    history_buffer.items[history_buffer.items.len - 1] = try line_buffer.toOwnedSlice(arena);
-                    row += 1;
-                    col_offset = 0;
-                    history_index = history_buffer.items.len;
-                    break :byte_loop;
                 },
                 control_code.del => {
                     if (col_offset == 0) {
@@ -112,27 +122,32 @@ pub fn main() !void {
                     try stdout_writer.writeByte(' ');
                     try cursor.setCursorColumn(stdout_writer, prompt.len + col_offset);
                 },
-                else => {
+                ' '...'~' => |print_byte| {
                     try cursor.setCursorColumn(stdout_writer, prompt.len);
                     try ansi_term.clear.clearFromCursorToLineEnd(stdout_writer);
 
                     if (col_offset < line_buffer.items.len) {
-                        line_buffer.items[col_offset] = byte;
+                        line_buffer.items[col_offset] = print_byte;
                     } else {
-                        try line_buffer.append(arena, byte);
+                        try line_buffer.append(arena, print_byte);
                     }
 
                     col_offset += 1;
                     try stdout_writer.writeAll(line_buffer.items);
                     try cursor.setCursorColumn(stdout_writer, prompt.len + col_offset);
                 },
+                else => |byte| try stderr_writer.print("Unhandled character: {c}\n", .{byte}),
             }
         }
     }
 }
 
 const std = @import("std");
+const control_code = std.ascii.control_code;
+
 const ansi_term = @import("ansi_term");
 const cursor = ansi_term.cursor;
+
+const string = @cImport(@cInclude("string.h"));
 const termios = @cImport(@cInclude("termios.h"));
-const control_code = std.ascii.control_code;
+// const errno = @cImport(@cInclude("errno.h"));
