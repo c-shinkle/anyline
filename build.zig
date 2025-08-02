fe0a985b84d7ee95000bce5de4a10a7bdc9d54b4
Christian Shinkle <cshinkle2018@gmail.com>
2025-08-02 15:47:26 -0500

    offically pulled code into a library

--- build.zig
+++ build.zig
@@ -4,6 +4,28 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const anyline_mod = b.createModule(.{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+
+    const is_x86_64_linux = target.result.cpu.arch == .x86_64 and target.result.os.tag == .linux;
+    const anyline_lib = b.addLibrary(.{
+        .name = "anyline",
+        .root_module = anyline_mod,
+        .use_llvm = !is_x86_64_linux,
+    });
+
+    const ansi_term_dep = b.dependency("ansi_term", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    anyline_lib.root_module.addImport("ansi_term", ansi_term_dep.module("ansi_term"));
+    b.installArtifact(anyline_lib);
+
     const exe_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
@@ -12,16 +34,13 @@ pub fn build(b: *std.Build) void {
     });
 
     const exe = b.addExecutable(.{
-        .name = "anyline",
+        .name = "anyline-exe",
         .root_module = exe_mod,
+        .use_llvm = !is_x86_64_linux,
     });
 
-    const ansi_term_dep = b.dependency("ansi_term", .{
-        .target = target,
-        .optimize = optimize,
-    });
-    exe.root_module.addImport("ansi_term", ansi_term_dep.module("ansi_term"));
-
+    exe.linkLibrary(anyline_lib);
+    exe.root_module.addImport("anyline", anyline_mod);
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
--- src/root.zig
+++ src/root.zig
@@ -1,13 +1,222 @@
-//! By convention, root.zig is the root source file when making a library. If
-//! you are making an executable, the convention is to delete this file and
-//! start with main.zig instead.
+pub fn readline(child_allocator: std.mem.Allocator, prompt: []const u8) ![]const u8 {
+    var col_offset: usize = 0;
+    var line_buffer = std.ArrayListUnmanaged(u8).empty;
+    // This dummy value guarentees there will always be an element at 'history_index'
+    // try history_entries.append(arena, undefined);
+    var edit_stack = std.ArrayListUnmanaged([]const u8).empty;
+
+    var arena_allocator = std.heap.ArenaAllocator.init(child_allocator);
+    defer arena_allocator.deinit();
+    const arena = arena_allocator.allocator();
+
+    const old = switch (builtin.os.tag) {
+        .linux => try Linux.init(),
+        .windows => try Windows.init(),
+        else => return error.UnsupportedOS,
+    };
+    defer switch (builtin.os.tag) {
+        .linux => old.deinit(),
+        .windows => old.deinit(),
+        else => unreachable,
+    };
+
+    const stdout_writer = std.io.getStdOut().writer();
+    const stderr_writer = std.io.getStdErr().writer();
+    const stdin_reader = std.io.getStdIn().reader();
+
+    try stdout_writer.writeAll(prompt);
+    if (builtin.os.tag == .windows) {
+        const console_input = std.io.getStdIn().handle;
+        if (0 == windows_c.FlushConsoleInputBuffer(console_input)) {
+            return error.SetConsoleModeFailure;
+        }
+    }
+
+    while (true) {
+        const first_byte = try stdin_reader.readByte();
+        switch (first_byte) {
+            CTRL_B => {
+                col_offset -|= 1;
+                try setCursorColumn(stdout_writer, prompt.len + col_offset);
+            },
+            CTRL_D => {
+                if (line_buffer.items.len == 0) {
+                    break;
+                }
+                if (line_buffer.items.len == col_offset) {
+                    continue;
+                }
+                const edited_line = try arena.dupe(u8, line_buffer.items);
+                try edit_stack.append(arena, edited_line);
+
+                _ = line_buffer.orderedRemove(col_offset);
+                try ansi_term.clear.clearFromCursorToLineEnd(stdout_writer);
+                try stdout_writer.writeAll(line_buffer.items[col_offset..]);
+                try setCursorColumn(stdout_writer, prompt.len + col_offset);
+            },
+            CTRL_F => {
+                col_offset = @min(col_offset + 1, line_buffer.items.len);
+                try setCursorColumn(stdout_writer, prompt.len + col_offset);
+            },
+            control_code.lf, control_code.cr => { // ENTER
+                // history_entries.items[history_index].edit_stack.clearRetainingCapacity();
+
+                // var last_entry = &history_entries.items[history_entries.items.len - 1];
+                // last_entry.line = try line_buffer.toOwnedSlice(arena);
+                // last_entry.edit_stack.clearRetainingCapacity();
+
+                // history_index = history_entries.items.len;
+                break;
+            },
+            control_code.esc => {
+                std.debug.assert('[' == try stdin_reader.readByte());
+                const third_byte = try stdin_reader.readByte();
+                switch (third_byte) {
+                    // UP_ARROW => {
+                    //     if (history_index == 0) {
+                    //         continue;
+                    //     }
+                    //
+                    //     if (history_index + 1 == history_entries.items.len) {
+                    //         const finished_line = try line_buffer.toOwnedSlice(arena);
+                    //         history_entries.items[history_index].line = finished_line;
+                    //     }
+                    //     history_entries.items[history_index].edit_stack = edit_stack;
+                    //
+                    //     history_index -|= 1;
+                    //
+                    //     line_buffer.clearRetainingCapacity();
+                    //     try line_buffer.appendSlice(
+                    //         arena,
+                    //         history_entries.items[history_index].line,
+                    //     );
+                    //     edit_stack = history_entries.items[history_index].edit_stack;
+                    //
+                    //     try setCursorColumn(stdout_writer, prompt.len);
+                    //     try ansi_term.clear.clearFromCursorToLineEnd(stdout_writer);
+                    //     try stdout_writer.writeAll(line_buffer.items);
+                    //     col_offset = line_buffer.items.len;
+                    // },
+                    // DOWN_ARROW => {
+                    //     if (history_index + 1 >= history_entries.items.len) {
+                    //         continue;
+                    //     }
+                    //
+                    //     history_entries.items[history_index].edit_stack = edit_stack;
+                    //     history_index += 1;
+                    //
+                    //     line_buffer.clearRetainingCapacity();
+                    //     try line_buffer.appendSlice(
+                    //         arena,
+                    //         history_entries.items[history_index].line,
+                    //     );
+                    //     edit_stack = history_entries.items[history_index].edit_stack;
+                    //
+                    //     try setCursorColumn(stdout_writer, prompt.len);
+                    //     try ansi_term.clear.clearFromCursorToLineEnd(stdout_writer);
+                    //     try stdout_writer.writeAll(line_buffer.items);
+                    //     col_offset = line_buffer.items.len;
+                    // },
+                    RIGHT_ARROW => {
+                        col_offset = @min(col_offset + 1, line_buffer.items.len);
+                        try setCursorColumn(stdout_writer, prompt.len + col_offset);
+                    },
+                    LEFT_ARROW => {
+                        col_offset -|= 1;
+                        try setCursorColumn(stdout_writer, prompt.len + col_offset);
+                    },
+                    DEL => {
+                        std.debug.assert('~' == try stdin_reader.readByte());
+
+                        if (line_buffer.items.len == col_offset) {
+                            continue;
+                        }
+
+                        const edited_line = try arena.dupe(u8, line_buffer.items);
+                        try edit_stack.append(arena, edited_line);
+
+                        _ = line_buffer.orderedRemove(col_offset);
+                        try ansi_term.clear.clearFromCursorToLineEnd(stdout_writer);
+                        try stdout_writer.writeAll(line_buffer.items[col_offset..]);
+                        try setCursorColumn(stdout_writer, prompt.len + col_offset);
+                    },
+                    else => {
+                        std.debug.print("Unhandled control byte: {d}\n", .{third_byte});
+                    },
+                }
+            },
+            UNDERLINE => {
+                if (edit_stack.items.len == 0) {
+                    continue;
+                }
+
+                line_buffer.clearRetainingCapacity();
+                try line_buffer.appendSlice(arena, edit_stack.pop().?);
+
+                try setCursorColumn(stdout_writer, prompt.len);
+                try ansi_term.clear.clearFromCursorToLineEnd(stdout_writer);
+                try stdout_writer.writeAll(line_buffer.items);
+                col_offset = line_buffer.items.len;
+            },
+            ' '...'~' => |print_byte| {
+                // If you are editting a line that was populated from history,
+                // then you need to include a backstop for undo's
+                // const is_last_entry = history_index + 1 == history_entries.items.len;
+                // if (!is_last_entry and edit_stack.items.len == 0) {
+                //     const duped_finished_line = try arena.dupe(u8, line_buffer.items);
+                //     try edit_stack.append(arena, duped_finished_line);
+                // }
+
+                try line_buffer.insert(arena, col_offset, print_byte);
+                try stdout_writer.writeAll(line_buffer.items[col_offset..]);
+
+                col_offset += 1;
+                try setCursorColumn(stdout_writer, prompt.len + col_offset);
+            },
+            BACK_SPACE => {
+                if (col_offset == 0) {
+                    continue;
+                }
+                const copied_line = try arena.dupe(u8, line_buffer.items);
+                try edit_stack.append(arena, copied_line);
+
+                col_offset -= 1;
+                _ = line_buffer.orderedRemove(col_offset);
+
+                try setCursorColumn(stdout_writer, prompt.len + col_offset);
+                try stdout_writer.writeAll(line_buffer.items[col_offset..]);
+                try stdout_writer.writeByte(' ');
+                try setCursorColumn(stdout_writer, prompt.len + col_offset);
+            },
+            else => |unknown_byte| try stderr_writer.print("Unhandled character: {d}\n", .{
+                unknown_byte,
+            }),
+        }
+    }
+
+    return try child_allocator.dupe(u8, line_buffer.items);
+}
+
+const CTRL_B = 0x02;
+const CTRL_D = 0x04;
+const CTRL_F = 0x06;
+const UP_ARROW = 'A';
+const DOWN_ARROW = 'B';
+const RIGHT_ARROW = 'C';
+const LEFT_ARROW = 'D';
+const DEL = '3';
+const UNDERLINE = 0x1F;
+const BACK_SPACE = 0x7F;
+
 const std = @import("std");
-const testing = std.testing;
+const builtin = @import("builtin");
+const control_code = std.ascii.control_code;
 
-pub export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
+const ansi_term = @import("ansi_term");
+const setCursorColumn = ansi_term.cursor.setCursorColumn;
+const clearFromCursorToLineEnd = ansi_term.clear.clearFromCursorToLineEnd;
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
-}
+const Linux = @import("Linux.zig");
+const Windows = @import("Windows.zig");
+
+const windows_c = @cImport(@cInclude("windows.h"));
